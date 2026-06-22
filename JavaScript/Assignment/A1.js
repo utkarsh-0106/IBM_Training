84b1dea215f1bf1089901956f47956ae57df2d71
utkarsh-0106 <utkarsh.maheshwari0106@gmail.com>
2026-06-22 14:54:53 +0530

    js

---
+++ JavaScript/Assignment/A1.js
@@ -0,0 +1,36 @@
+var A = 12
+var B = 12
+var C = 12
+var N = 4321
+
+// Q1. Add Two Numbers
+let result1 = A + B;
+console.log(result1);
+
+// Q2. Find if the conditions are obeyed or not?
+console.log((A < 10 && A > B));
+
+// Q3. Check the Conditions
+console.log((A % 10 === 0 || B % 10 === 0));
+
+// Q4. Find the First Digit of a 4-Digit Number
+console.log(parseInt(N / 1000));
+
+// Q5. Find the Last Digit of a 4-Digit Number
+console.log(N % 10);
+
+// Q6. Find the Remainder
+console.log(B % A);
+
+// Q7. Multiply Two Numbers
+console.log(A * B);
+
+// Q8. Marks Calculator
+
+// Sum
+let sum = A + B + C;
+console.log(sum);
+
+// Average
+let avg = ((A + B + C) / 3).toFixed(2);
+console.log(avg);
---
+++ JavaScript/jsday1.js
@@ -0,0 +1,31 @@
+console.log("KONICHIWA SENSEI");
+
+console.log(4+2);
+console.log(4*2);
+console.log(4/2);
+console.log(4%2);
+console.log(4-2);
+
+
+// console.log(a);
+// console.log(d);
+
+// var a = "hey hey" //can be reassign and redeclare
+// const d = "balushai" //cannot reassign 
+// let m  = "onniechan" //can be reassign cannot redeclare
+
+// a = 21
+
+// console.log(a);
+
+// console.log(d);
+// console.log(m);
+
+
+// var a = 30
+// let b = "30"
+// let res1 = a == b
+// let res2 = a === b
+// console.log(res1);
+// console.log(res2);
+
