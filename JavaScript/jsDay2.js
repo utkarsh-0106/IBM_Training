65f1895414e3ea02d4b03b91a7b24f909953c6f4
utkarsh-0106 <utkarsh.maheshwari0106@gmail.com>
2026-06-23 15:05:51 +0530

    day2

---
+++ JavaScript/jsDay2.js
@@ -0,0 +1,74 @@
+// let x = 7;
+
+// if(x <= 5) {
+//     console.log("hey less than 5");
+// }
+// else console.log("more than 5");
+
+// let val = parseInt(prompt("enter day value"));
+
+// switch(val) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+
+//     case 4:
+//         console.log("thursday");
+//         break;
+
+//     case 5:
+//         console.log("friday hai partyyyyy");
+//         break;
+
+//     case 6:
+//         console.log("saturday");
+//         break;
+
+//     case 7:
+//         console.log("sunday");
+//         break;
+
+//     default:
+//         console.log("invalid");
+// }
+
+// let marks = parseInt(prompt("enter marks"));
+
+// switch(true) {
+
+//     case marks <= 10:
+//         console.log("E");
+//         break;
+
+//     case marks <= 20 && 11 <= marks:
+//         console.log("D");
+//         break;
+
+//     case  marks <= 30 && 21 <= marks:
+//         console.log("C");
+//         break;
+
+//     case marks>=31 && marks <= 40:
+//         console.log("B");
+//         break;
+
+//     case marks>40:
+//         console.log("A");
+//         break;
+
+//     default:
+//         console.log("invalid");
+      
+// }
+
+for(let i = 1; i <= 10; i++) {
+    console.log("19 *", i, "=", 19 * i);;
+}
